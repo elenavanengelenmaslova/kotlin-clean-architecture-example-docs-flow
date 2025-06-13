0e889a76cf19ff89a789a77d09144fb2a006ba05
elena.vanengelen <elena.vanengelen@edsn.nl>
2025-06-13 12:33:45 +0200

    init demo

--- software/application/src/main/kotlin/com/example/clean/architecture/persistence/ObjectStorageInterface.kt
+++ software/application/src/main/kotlin/com/example/clean/architecture/persistence/ObjectStorageInterface.kt
@@ -1,6 +1,9 @@
 package com.example.clean.architecture.persistence
 
 interface ObjectStorageInterface {
+    /**
+     * Save content to storage
+     */
     fun save(id: String, content: ByteArray): String
     /**
      * Generates a secure access URI for the given blob ID.
